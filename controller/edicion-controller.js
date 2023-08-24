9ce8fea1ed7c9833345b69839d23a0aaa9ddc232
julianddress <andres.mtta1@gmil.com>
2023-08-23 23:50:31 -0500

    USING ASYNC AND AWAIT INSTEAD OF THEN AND CATCH

--- controller/edicion-controller.js
+++ controller/edicion-controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
 
@@ -10,15 +10,22 @@ const obtenerInformacion = () => {
         window.location.href = "/screens/error.html"
     }
 
-    clientServices.detalleCliente(id).then( (perfil) => {
-        nombre.value = perfil.nombre;
-        email.value = perfil.email;
-    });
+    try{
+        const perfil = await clientServices.detalleCliente(id);
+        if(perfil.nombre && perfil.email){
+            nombre.value = perfil.nombre;
+            email.value = perfil.email;
+        }else{
+            throw new Error();
+        }
+    }catch(error){
+        window.location.href = "/screens/error.html"
+    }
 }
 
 obtenerInformacion();
 
-formulario.addEventListener("submit", (evento) => {
+formulario.addEventListener("submit",  async (evento) => {
     evento.preventDefault();
 
     const url = new URL(window.location);
@@ -27,7 +34,6 @@ formulario.addEventListener("submit", (evento) => {
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email]").value;
 
-    clientServices.actualizarCliente(nombre, email, id).then(() => {
-        window.location.href = "/screens/edicion_concluida.html"
-    })
+    const data = await clientServices.actualizarCliente(nombre, email, id);
+    window.location.href = "/screens/edicion_concluida.html"
 })
